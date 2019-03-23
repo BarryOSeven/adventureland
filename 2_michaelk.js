e7cdef3bdb20f722e6001d5e738a9ec1c0dfc806
Wouter Rengelink <wouter@Wouters-MacBook-Pro.local>
2019-03-23 17:08:21 +0100

    Back to default movement.

--- 2_michaelk.js
+++ 2_michaelk.js
@@ -79,7 +79,11 @@ setInterval(function(){
 
 	if(!in_attack_range(target))
 	{
-		smart_move({to: target});
+		move(
+			character.x+(target.x-character.x)/2,
+			character.y+(target.y-character.y)/2
+		);
+		// Walk half the distance
 	}
 	else if(can_attack(target))
 	{
