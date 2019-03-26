07e2b8adecffb5a1863e0d4d406fdaa3c0b5a61b
BarryOSeven <barryoseven@gmx.com>
2019-03-26 16:34:46 +0100

    Splitted github code from jafarm code.

--- 1_jafarm.js
+++ 1_jafarm.js
@@ -1,5 +1,9 @@
 //JafarM
 
+load_code(97, function() {
+	game_log("Unable to run updates");
+});
+
 load_code(98, function() {
 	game_log("Unable to run generics");
 });
@@ -20,7 +24,7 @@ add_top_button("log", "Log", () => {
 function on_code_updated() {
 	start_character("MichaelK", 2);
 	start_character("Leonidas", 3);
-	start_character("BarryOSeven", 4);	
+	start_character("BarryOSeven", 4);
 }
 
 function on_destroy() {
@@ -47,72 +51,10 @@ function buy_upgrade_scrolls() {
 	}
 }
 
-pull_code(on_code_updated);
-
 setInterval(function() {
-    if (character.rip) {
-		respawn();	
-    }
-    
 	if (is_moving(character)) {
 		return;
     }
     
     buy_upgrade_scrolls();
 }, 1000);
-
-// github
-const baseURL = "http://localhost/";
-
-const allFiles = [
-    "1_jafarm.js",
-    "2_michaelk.js",
-    "3_leonidas.js",
-    "4_barryoseven.js",
-    "98_generics.js",
-    "99_monster_determinator.js"
-];
-
-function pull_code(on_code_updated) {
-	let updated = 0;
-    parent.api_call("list_codes", {
-        callback: function () {
-            game_log("Updating from GitHub...");
-            for (let file of allFiles) {
-                let request = new XMLHttpRequest();
-                request.open("GET", baseURL + file);
-                request.onreadystatechange = function () {
-                    if (request.readyState === 4 && request.status === 200) {
-                        let codeObject = getCodeObject(file);
-                        let data = {
-                            name: codeObject.name,
-                            slot: codeObject.slot,
-                            code: request.responseText
-                        };
-                        
-						parent.api_call("save_code", data);
-						updated++;
-						if (updated === allFiles.length) {
-							game_log("Code succesfully updated");
-							on_code_updated();
-						}
-                    }
-                };
-                request.send();
-            }
-        }
-    });
-}
-
-function getCodeObject(file) {
-    let codeObject;
-
-    let arr = file.substring(0, file.length - 3).split("_");
-
-    codeObject = {
-        slot: arr[0],
-        name: arr[1]
-    };
-
-    return codeObject;
-}
---
+++ 97_update.js
@@ -0,0 +1,57 @@
+const baseURL = "http://localhost/";
+
+const allFiles = [
+    "1_jafarm.js",
+    "2_michaelk.js",
+    "3_leonidas.js",
+    "4_barryoseven.js",
+    "97_update.js",
+    "98_generics.js",
+    "99_monster_determinator.js"
+];
+
+function pull_code() {
+	let updated = 0;
+    parent.api_call("list_codes", {
+        callback: function () {
+            game_log("Updating code from server...");
+            for (let file of allFiles) {
+                let request = new XMLHttpRequest();
+                request.open("GET", baseURL + file);
+                request.onreadystatechange = function () {
+                    if (request.readyState === 4 && request.status === 200) {
+                        let codeObject = getCodeObject(file);
+                        let data = {
+                            name: codeObject.name,
+                            slot: codeObject.slot,
+                            code: request.responseText
+                        };
+                        
+						parent.api_call("save_code", data);
+						updated++;
+						if (updated === allFiles.length) {
+							game_log("Code succesfully updated");
+							on_code_updated();
+						}
+                    }
+                };
+                request.send();
+            }
+        }
+    });
+}
+
+function getCodeObject(file) {
+    let codeObject;
+
+    let arr = file.substring(0, file.length - 3).split("_");
+
+    codeObject = {
+        slot: arr[0],
+        name: arr[1]
+    };
+
+    return codeObject;
+}
+
+pull_code();
