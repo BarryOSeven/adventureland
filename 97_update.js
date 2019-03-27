3c04216d3064477f12507b85c6ed03cd4edcf4bf
BarryOSeven <wrengelink@me.com>
2019-03-27 09:13:54 +0100

    Implemented server check.

--- 97_update.js
+++ 97_update.js
@@ -11,24 +11,42 @@ const allFiles = [
 ];
 
 function on_code_updated() {
-    start_character("JafarM", 1);
+    	start_character("JafarM", 1);
 	start_character("MichaelK", 2);
 	start_character("Leonidas", 3);
 	start_character("BarryOSeven", 4);
 }
 
 function on_destroy() {
-    stop_character("JafarM");
+    	stop_character("JafarM");
 	stop_character("MichaelK");
 	stop_character("Leonidas");
-    stop_character("BarryOSeven");
+    	stop_character("BarryOSeven");
+}
+
+function do_server_check(on_server_up, on_server_down) {
+	game_log("Performing update server check");
+	const request = new XMLHttpRequest();
+	request.open("GET", baseURL + allFiles[0]);
+	request.onreadystatechange = function () {
+	    if (request.readyState === 4 && request.status === 200) {
+		game_log("Server up: pulling code");
+		on_server_up();
+	    } else
+	    if (request.readyState === 4) {
+		game_log("Server down: continuing");
+	        on_server_down();
+	    }
+	}
+	request.send();
 }
 
 function pull_code(on_code_updated) {
-	let updated = 0;
+    let updated = 0;
     parent.api_call("list_codes", {
         callback: function () {
             game_log("Updating code from server...");
+		
             for (let file of allFiles) {
                 let request = new XMLHttpRequest();
                 request.open("GET", baseURL + file);
@@ -68,4 +86,12 @@ function getCodeObject(file) {
     return codeObject;
 }
 
-pull_code(on_code_updated);
+function on_server_up() {
+    pull_code(on_code_updated);	
+}
+	
+function on_server_down() {
+    on_code_updated();
+}
+
+do_server_check(on_server_up, on_server_down);
