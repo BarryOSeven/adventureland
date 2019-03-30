1c1b5a6918a2c36a5f63d10e80248345b9b1bde0
BarryOSeven <barryoseven@gmx.com>
2019-03-30 02:47:08 +0100

    Only attack targeted type.

--- 2_michaelk.js
+++ 2_michaelk.js
@@ -83,7 +83,9 @@ setInterval(function() {
 	let target=get_targeted_monster();
 	
 	if (!target) {
-		target = get_nearest_monster();
+		target = get_nearest_monster({
+			type: monster_type
+		});
 		// target = get_nearest_hostile({
 		// 	exclude: names
 		// });
